48cd65d9241aa68de093edf09089b5b63ffecb90
Andy Chan <github@sockbot.com>
2020-09-12 14:38:22 -0700

    declare server variable

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
