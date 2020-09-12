189a9dcdfc3f6e2c972cf06d7303108d76c3eeb3
Seongchan Lee <seongchan.lee96@gmail.com>
2020-09-12 11:45:49 -0700

    Hello World setup

---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello World'),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
