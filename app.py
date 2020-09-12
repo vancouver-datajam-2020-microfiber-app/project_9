2f1ae743e13a0748b9aa7c419f1b7081c5908545
Andy Chan <github@sockbot.com>
2020-09-12 16:27:10 -0700

    create dataframe from csv

--- app.py
+++ app.py
@@ -15,28 +15,22 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv('Data/microfibre_loss_per_type.csv')
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.bar(df, x='textile_type', y='avg')
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1('Hello Dash'),
 
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
-
     dcc.Graph(
         id='example-graph',
         figure=fig
     )
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
